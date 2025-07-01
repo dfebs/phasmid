a94c7f518dfc978cd91a2743b77201a3b15114ae
dfebs <daniel.febles97@gmail.com>
2025-06-30 23:08:50 -0400

    Migrate to add usernames to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_01_011629) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_01_023909) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -63,7 +63,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_01_011629) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
