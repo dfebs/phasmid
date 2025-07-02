56a1bb447c4a0c4b76377180def9a4682f328ae3
dfebs <daniel.febles97@gmail.com>
2025-07-01 22:08:56 -0400

    Migrate for bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_01_220354) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_02_013736) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -49,6 +49,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_01_220354) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "bugs", force: :cascade do |t|
+    t.string "title"
+    t.integer "author_id", null: false
+    t.integer "project_id", null: false
+    t.string "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_bugs_on_author_id"
+    t.index ["project_id"], name: "index_bugs_on_project_id"
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -79,6 +90,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_01_220354) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bugs", "projects"
+  add_foreign_key "bugs", "users", column: "author_id"
   add_foreign_key "projects", "users", column: "owner_id"
   add_foreign_key "sessions", "users"
 end
