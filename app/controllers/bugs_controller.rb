aba09e3c99c9fbce01333bc8b4ca4fd265cdbeda
dfebs <daniel.febles97@gmail.com>
2025-07-01 23:42:35 -0400

    Create bugs in projects

---
+++ app/controllers/bugs_controller.rb
@@ -0,0 +1,32 @@
+class BugsController < ApplicationController
+  def new
+    @bug = Bug.new
+    @project = Project.find(params[:project_id])
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @bug = @project.bugs.new(bug_params)
+    @bug.author = Current.user
+
+
+    respond_to do |format|
+      if @bug.save
+        format.html { redirect_to @project, notice: "Bug was successfully created." }
+      else
+
+        flash.now[:alert] = "Failed to create bug"
+        format.html { render "projects/show", status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def set_bug
+    @bug = Bug.find(params.expect(:id))
+  end
+
+  def bug_params
+    params.expect bug: [ :title, :description ]
+  end
+end
