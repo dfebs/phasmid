8cc9204a05f4dd8e8df607079a469722fed3087a
dfebs <daniel.febles97@gmail.com>
2025-07-03 19:00:35 -0400

    Fully fix project controller test

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -6,16 +6,19 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
+    post session_url(email_address: users(:user_one).email_address, password: "420SirPassingtonXIV_WEOUTHERE")
     get projects_url
     assert_response :success
   end
 
   test "should get new" do
+    post session_url(email_address: users(:user_one).email_address, password: "420SirPassingtonXIV_WEOUTHERE")
     get new_project_url
     assert_response :success
   end
 
   test "should create project" do
+    post session_url(email_address: users(:user_one).email_address, password: "420SirPassingtonXIV_WEOUTHERE")
     assert_difference("Project.count") do
       post projects_url, params: { project: { description: @project.description, title: @project.title } }
     end
@@ -24,21 +27,25 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show project" do
+    post session_url(email_address: users(:user_one).email_address, password: "420SirPassingtonXIV_WEOUTHERE")
     get project_url(@project)
     assert_response :success
   end
 
   test "should get edit" do
+    post session_url(email_address: users(:user_one).email_address, password: "420SirPassingtonXIV_WEOUTHERE")
     get edit_project_url(@project)
     assert_response :success
   end
 
   test "should update project" do
+    post session_url(email_address: users(:user_one).email_address, password: "420SirPassingtonXIV_WEOUTHERE")
     patch project_url(@project), params: { project: { description: @project.description, title: @project.title } }
     assert_redirected_to project_url(@project)
   end
 
   test "should destroy project" do
+    post session_url(email_address: users(:user_one).email_address, password: "420SirPassingtonXIV_WEOUTHERE")
     assert_difference("Project.count", -1) do
       delete project_url(@project)
     end
