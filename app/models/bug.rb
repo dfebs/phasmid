146aca7fb75901609406fad832a5ca991122277f
dfebs <daniel.febles97@gmail.com>
2025-07-13 12:03:28 -0400

    Show bugs by the user in the left sidebar

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -5,5 +5,5 @@ class Bug < ApplicationRecord
   has_many :bug_tags, dependent: :destroy
   has_many :tags, through: :bug_tags, dependent: :destroy
 
-  after_create_commit -> { broadcast_prepend_to "project_bugs_sub_#{self.project.id}", partial: "bugs/bug", locals: { bug: self, project: self.project }, target: "bugs" }
+  after_create_commit -> { broadcast_prepend_to "project_bugs_sub_#{self.project.id}", partial: "bugs/bug", locals: { bug: self, project: self.project, type: "main" }, target: "bugs" }
 end
